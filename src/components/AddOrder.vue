5834489d558f66349520b47215419a2a05b21555
muratcan-yuksel <muratcanyukselpro@gmail.com>
2021-11-08 17:33:24 +0300

    fixed the stupid error

--- src/components/AddOrder.vue
+++ src/components/AddOrder.vue
@@ -1,76 +1,169 @@
 <template>
-  <div class="home">
-    <h1>Siparişler</h1>
-    <!-- <p>{{ orders }}</p> -->
-    <div v-if="orders.length > 0">
-      <div class="order" v-for="(order, index) in orders" :key="index">
-        <p>Mağaza adı : {{ order.mağazaAdı }}</p>
-        <p>Ürün Adı: {{ order.ürünAdı }}</p>
-        <p>Boyut: {{ order.boyut }}</p>
-        <p>Kişiselleştirme: {{ order.kişiselleştirme }}</p>
-        <p>Renk: {{ order.renk }}</p>
-        <p>Ürün Adedi: {{ order.ürünAdedi }}</p>
-        <p>Siparişin verildiği tarih: {{ order.siparişTarihi }}</p>
-        <p>Müşteri Notu: {{ order.müşteriNotu }}</p>
-        <b-button class="btn" @click="handleDelete" variant="danger"
-          >Siparişi Sil</b-button
-        >
-        <!-- hide the element from the user -->
-        <p v-show="showing">{{ order.id }}</p>
+  <div>
+    <div v-if="display">
+      <h1 id="title">Gist Leather Craft</h1>
+      <h1 id="title">Sipariş Sistemi</h1>
+      <!-- mağazaAdı -->
+      <FormulateInput
+        type="text"
+        label="Mağaza adı"
+        v-model="mağazaAdı"
+        validation="required"
+      />
+      <!-- ürünAdı -->
+      <FormulateInput
+        type="text"
+        label="Sipariş edilecek ürün"
+        v-model="ürünAdı"
+        validation="required"
+      />
+      <!-- dosya ekleme -->
+      <div class="inputFields">
+        <div class="inputs">
+          <h5>Ürün fotoğrafı</h5>
+          <input @change="handleChange" type="file" />
+        </div>
+        <div class="inputs">
+          <h5>Barkod</h5>
+          <input @change="handlePDF" type="file" />
+        </div>
       </div>
+
+      <!-- ürün rengi -->
+      <div>
+        <b-dropdown id="dropdown-1" :text="renk" class="m-md-2">
+          <b-dropdown-item @click="handleColor">Brown</b-dropdown-item>
+          <b-dropdown-item @click="handleColor">Dark Brown</b-dropdown-item>
+          <b-dropdown-item @click="handleColor">Grey</b-dropdown-item>
+          <b-dropdown-item @click="handleColor">Black</b-dropdown-item>
+          <b-dropdown-item @click="handleColor">Crazy Brown</b-dropdown-item>
+          <b-dropdown-item @click="handleColor"
+            >Crazy Dark Brown</b-dropdown-item
+          >
+          <b-dropdown-item @click="handleColor">Crazy Grey</b-dropdown-item>
+          <b-dropdown-item @click="handleColor">Özel</b-dropdown-item>
+        </b-dropdown>
+      </div>
+      <!-- boyut -->
+      <FormulateInput type="text" label="Boyut" v-model="boyut" />
+      <!-- kişiselleştirme -->
+      <FormulateInput
+        type="text"
+        label="Kişiselleştirme"
+        v-model="kişiselleştirme"
+      />
+      <!-- ürün adedi -->
+      <FormulateInput
+        type="number"
+        label="Sipariş edilecek ürün adedi"
+        v-model="adet"
+        validation="required"
+      />
+      <!-- not -->
+      <FormulateInput
+        type="textarea"
+        label="Eklemek istediğiniz not"
+        v-model="not"
+      />
+      <b-button class="btn" @click="handleInput" variant="success"
+        >Siparişi Onayla</b-button
+      >
     </div>
-    <div v-else>
-      <h2>Gösterilecek sipariş bulunmamaktadır.</h2>
+    <div class="success" v-else>
+      <h2>
+        Siparişiniz bize ulaşmıştır. En kısa zamanda verdiğiniz bilgilerle
+        sizinle iletişime geçeceğiz.
+      </h2>
+      <h2>
+        Gist Leather Craft El Sanatları Atölyesi ile çalışmayı seçtiğiniz için
+        teşekkür ederiz.
+      </h2>
     </div>
   </div>
 </template>
 
 <script>
-//import firebase database
-import { db } from "../firebase/db";
+import { db, app } from "../firebase/db";
 export default {
-  name: "Home",
-  components: {},
   data() {
     return {
-      orders: [],
-      orderId: "",
-      showing: false,
+      mağazaAdı: "",
+      ürünAdı: "",
+      adet: 0,
+      not: "",
+      file: "",
+      fileName: "",
+      pdf: "",
+      pdfName: "",
+      boyut: "",
+      kişiselleştirme: "",
+      renk: "Renk",
+      display: true,
     };
   },
-  firestore: {
-    orders: db.collection("orders").orderBy("timeStamp", "asc"),
-  },
-  mounted() {
-    // db.collection("orders").doc("12").set({ name: "namesdf" });
-    // db.collection("orders").doc("12").delete();
-    console.log("This component's unique id is: " + this.uid);
-    // console.log(new Date().toLocaleString());
-  },
   methods: {
-    handleDelete(e) {
-      //get the textcontent element next to the button
-      //which is its id in firestore
-      console.log(e.target.nextElementSibling.textContent);
-      // console.log(this.$parent);
-      //delete selected item from firebase and display at the same time
-      db.collection("orders")
-        .doc(e.target.nextElementSibling.textContent)
-        .delete();
+    async handleChange(e) {
+      const file = e.target.files[0];
+      const storageRef = app.ref();
+      const fileRef = storageRef.child(file.name);
+      await fileRef.put(file);
+      this.file = await fileRef.getDownloadURL();
+      this.fileName = e.target.files[0].name;
+    },
+    async handlePDF(e) {
+      const file = e.target.files[0];
+      const storageRef = app.ref();
+      const fileRef = storageRef.child(file.name);
+      await fileRef.put(file);
+      this.pdf = await fileRef.getDownloadURL();
+      this.pdfName = e.target.files[0].name;
+    },
+    handleInput() {
+      if (this.mağazaAdı != "" && this.ürünAdı != "" && this.adet != 0) {
+        db.collection("orders").doc().set({
+          mağazaAdı: this.mağazaAdı,
+          ürünAdı: this.ürünAdı,
+          ürünAdedi: this.adet,
+          boyut: this.boyut,
+          kişiselleştirme: this.kişiselleştirme,
+          müşteriNotu: this.not,
+          file: this.file,
+          fileName: this.fileName,
+          pdf: this.pdf,
+          pdfName: this.pdfName,
+          renk: this.renk,
+          siparişTarihi: new Date().toLocaleString(),
+          timeStamp: Date.now(),
+        });
+        this.display = false;
+      } else {
+        alert("lütfen bütün gerekli alanları doldurunuz!");
+      }
+    },
+    handleColor(e) {
+      console.log(e.target.textContent);
+      this.renk = e.target.textContent;
     },
   },
 };
 </script>
 <style scoped>
-.title {
-  font-weight: 700;
+#title {
+  /* margin-left: 4em;
+  margin-right: 4em; */
 }
-.order {
-  border: 2px solid black;
-  background-color: rgb(235, 228, 228);
-  margin: 2em;
+/* .inputFields {
+  display: flex;
+  justify-content: space-around;
+} */
+.inputs {
+  margin: 10px;
+}
+.success {
+  margin: 5em;
 }
 .btn {
-  margin: 2em;
+  margin-bottom: 2em;
+  margin-top: 1em;
 }
 </style>
